3c2b897997deae849f650f7d3ed6145e38efbff0
Scoutzknifez <codygaming@4alexanders.com>
2020-04-26 20:42:11 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='AutoInput',
+    version='1.0',
+    packages=[''],
+    # url='https://github.com/ConnorWolanski/TeamFightTacticsBot.git',
+    license='',
+    requires=['aenum', 'pyscreenshot', 'pygame', 'pynput'],
+    author='Cody Alexander',
+    author_email='csalexander<at>cpp.edu',
+    description=''
+)
