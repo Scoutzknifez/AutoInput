e7cfea241e375d03dbddb164328d1dc0dc4e923e
Scoutzknifez <codygaming@4alexanders.com>
2020-04-29 14:47:12 -0700

    Added auto conversion based on time

--- AutoInput/Main.py
+++ AutoInput/Main.py
@@ -8,10 +8,24 @@ def start():
         return
 
     time.sleep(3)
+    loop_sleep_time = 30
+
+    # Conversion
+    converting = True
+    seconds_before_convert = 3600 / 3
+    convert_loop_allowance = seconds_before_convert / loop_sleep_time
+
+    loop_count = 0
 
     while True:
         Utils.press_and_release(Constants.HEX_DICTIONARY['space'], .05)
-        time.sleep(30)
+        loop_count += 1
+
+        if converting and loop_count > convert_loop_allowance:
+            Utils.press_and_release(Constants.HEX_DICTIONARY['2'], .01)
+            loop_count = 0
+
+        time.sleep(loop_sleep_time)
 
 
 start()
