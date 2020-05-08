dd4f84245c37d317701c4dbd14d1c4471ca700bc
Scoutzknifez <codygaming@4alexanders.com>
2020-05-07 23:39:50 -0700

    Added some more keys to the config file

--- AutoInput/BeeSwarmSimulator/Config.py
+++ AutoInput/BeeSwarmSimulator/Config.py
@@ -3,12 +3,24 @@ NOT_AFK_KEY = 'space'
 loop_sleep_time = 60
 loop_count = 0
 
-# Conversion Statics
+# Sprinkler Settings
+SPRINKLER_KEY = '1'
+
+# Conversion Settings
 CONVERSION_KEY = '2'
 converting = True
-seconds_before_convert = 3600 / 1.5
+seconds_before_convert = 3600
 convert_loop_allowance = seconds_before_convert / loop_sleep_time
+
+# Field Dice Settings
+FIELD_DICE_KEY = '3'
+
+# Gumdrop Settings
+GUMDROP_KEY = '4'
 
 # Marshmallow Bee Settings
 MARSHMALLOW_BEE_KEY = '5'
 MARSHMALLOW_BEE_COOLDOWN = 1800  # Seconds (Half an hour)
+
+# Jellybean Settings
+JELLYBEAN_KEY = '6'
